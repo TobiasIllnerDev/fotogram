c74fd7780162274a1ee0015f45c7d852ffe0aab3
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-05-05 12:31:14 +0200

    footer created

---
+++ script.js
@@ -0,0 +1,29 @@
+let myImgs = [
+    "bird.png",
+    "bird_on_stone.png",
+    "cat.png",
+    "city_night.png",
+    "clouds_from_top.png",
+    "duck.png",
+    "lake_with_mountain.png",
+    "men_in_night.png",
+    "mountain_with_snow.png",
+    "rainy_clouds.png",
+    "sky_with_mountain.png",
+    "tree_with_snow.png"
+];
+
+let myImgsDescription = [
+    "Bird",
+    "Bird on stone",
+    "Cat",
+    "City at Night",
+    "Clouds from Space",
+    "duck",
+    "lake with Mountains",
+    "Men at Night",
+    "Mountain with snow",
+    "Rainy clouds",
+    "Sky with Mountains",
+    "Tree with snow"
+];
