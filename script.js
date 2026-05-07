a58aeccfb0fbde56eac97c786126eae91f2c740d
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-05-07 16:58:20 +0200

    Added secound Section

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ const myImgs = [
     "./assets/img/flower.png",
     "./assets/img/fox.png",
     "./assets/img/jellyfish.png",
+
+];
+const myImgsSecound = [
     "./assets/img/mountain.png",
     "./assets/img/owl.png",
     "./assets/img/ozean.png",
     "./assets/img/snow_mountain.png",
     "./assets/img/swan.png",
     "./assets/img/highland_cow.png"
-];
+]
 const myImgsDescription = [
     "Cherry Blossem",
     "City",
@@ -19,14 +22,17 @@ const myImgsDescription = [
     "Flowers",
     "Fox",
     "Jellyfish",
+];
+const myImgsDescriptionSecound = [
     "Mountains",
     "Owl",
     "Ozean",
     "Snow Mountains",
     "Swan",
     "Highland Cow"
-];
+]
 let currentIndex = 0;
+let currentIndexSecound = 0;
 
 function render() {
     const contentRef = document.getElementById('main_img_section');
@@ -78,6 +84,62 @@ function updateDialog() {
 
 function closeDialog() {
     let dialog = document.getElementById("myDialog");
+    dialog.classList.remove("opened"); 
+    dialog.close();
+}
+
+// Secound
+
+function renderSecound() {
+    const contentRefSecound = document.getElementById('main_img_section_secound');
+    for(let j = 0; j < myImgsSecound.length; j++){
+        contentRefSecound.innerHTML += getNotesHTMLSecound(j);
+    }
+}
+
+function getNotesHTMLSecound(j){
+    return `
+        <div class="img_for_main_secound">
+            <button onclick="openDialog(${j})"><img src="${myImgsSecound[j]}" 
+                alt="${myImgsDescriptionSecound[j]}" />
+            </button>
+        </div>
+    `;
+}
+
+function openDialogSecound(j) {
+    currentIndexSecound = j;
+    let dialog = document.getElementById("myDialogSecound");
+    let img = document.getElementById("dialogImgSecound");
+    let text = document.getElementById("dialogTextSecound");
+    let counter = document.getElementById("dialogCounterSecound");
+
+    img.src = myImgsSecound[currentIndexSecound];
+    text.innerText = myImgsDescriptionSecound[currentIndexSecound];
+    counter.innerText = `${currentIndexSecound + 1}/${myImgsSecound.length}`;
+
+    dialog.classList.add("opened");
+    dialog.showModal();
+}
+
+function nextPictureSecound() {
+    currentIndexSecound = (currentIndexSecound + 1) % myImgsSecound.length;
+    updateDialogSecound();
+}
+
+function backPictureSecound() {
+    currentIndexSecound = (currentIndexSecound - 1 + myImgsSecound.length) % myImgsSecound.length;
+    updateDialogSecound();
+}
+
+function updateDialogSecound() {
+    document.getElementById("dialogImgSecound").src = myImgsSecound[currentIndexSecound];
+    document.getElementById("dialogTextSecound").innerText = myImgsDescriptionSecound[currentIndexSecound];
+    document.getElementById("dialogCounterSecound").innerText = `${currentIndexSecound + 1}/${myImgsSecound.length}`;
+}
+
+function closeDialog() {
+    let dialog = document.getElementById("myDialogSecound");
     dialog.classList.remove("opened"); 
     dialog.close();
 }
